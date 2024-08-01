024e95ab30042a2fd5c999c20c731e5082fd6287
hyorim jo <133748490+hyorim-jo@users.noreply.github.com>
2024-08-01 19:44:41 +0900

    버튼 동작 성공, 레이어 순서 조정 필요

--- scripts/login.js
+++ scripts/login.js
@@ -1,23 +1,23 @@
 document.addEventListener("DOMContentLoaded", function() {
     const user = document.getElementById("user");
-    const container = document.querySelectorAll(".container");
+    const container = document.querySelector(".container");
+    const overlay = document.querySelector(".overlay");
 
+    /*버튼을 두 번 눌러야 떠서 none 추가*/
+    container.style.display = "none";
+    overlay.style.display = "none";
 
-    user.addEventListener("click", function() 
-    {
-        // Toggle the display of container
-        if (container.style.display === "none" || container.style.display === "") {
+    user.addEventListener("click", function() {
+        if (container.style.display === "none") {
             container.style.display = "block";
         } else {
             container.style.display = "none";
         }
 
-        // Toggle the display of overlay
-        if (overlay.style.display === "none" || overlay.style.display === "") {
+        if (overlay.style.display === "none") {
             overlay.style.display = "block";
         } else {
             overlay.style.display = "none";
         }
     });
-
 });
