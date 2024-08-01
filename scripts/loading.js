f9728cb963f55f5412f24b334477963267f202bd
ian <hope030327@naver.com>
2024-08-01 23:11:04 +0900

    범인 화면 거의 완성 (아직 범인 대사 작성 X)

---
+++ scripts/loading.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // 로딩 화면을 3초 동안 표시한 후 criminal.html로 이동하기
+    setTimeout(function() {
+        window.location.href = "criminal.html";
+    }, 3000);
+});
