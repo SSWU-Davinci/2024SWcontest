170cc1155a2f480d9263030c42770a0d4aa1e186
Ian <145183497+2anizirong@users.noreply.github.com>
2024-08-02 13:47:16 +0900

    로딩 / 게임 시작 추가 수정  (#21)
    * game start 추가 수정
    
    * 로딩중 창 변경

--- scripts/startmainscript.js
+++ scripts/startmainscript.js
@@ -1,7 +1,8 @@
 function handleKeyPress(event) {
     if (event.code === 'Space' || event.code === 'Enter') {
-        // gameScreen.html로 페이지 이동 --> 이건 내가 임의로 설정한 파일이름임!!!! 엔터나 스페이스바 치면 게임 화면으로 넘어감
-        window.location.href = 'gameScreen.html';
+        // 엔터나 스페이스바 누르면 로딩중 화면으로 이동
+        // 아직 확실히 결정된건 아님!!!!
+        window.location.href = 'loading.html';
     }
 }
 document.addEventListener('keydown', handleKeyPress);
