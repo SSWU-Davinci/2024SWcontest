60c8e235741c570b5eaf8580d46361e635e4fab6
ian <hope030327@naver.com>
2024-08-02 13:22:18 +0900

    game start 추가 수정

--- scripts/startmainscript.js
+++ scripts/startmainscript.js
@@ -1,7 +1,7 @@
 function handleKeyPress(event) {
     if (event.code === 'Space' || event.code === 'Enter') {
-        // gameScreen.html로 페이지 이동 --> 이건 내가 임의로 설정한 파일이름임!!!! 엔터나 스페이스바 치면 게임 화면으로 넘어감
-        window.location.href = 'gameScreen.html';
+        // 엔터나 스페이스바 누르면 홈 게임화면으로 이동
+        window.location.href = 'home.html';
     }
 }
 document.addEventListener('keydown', handleKeyPress);
