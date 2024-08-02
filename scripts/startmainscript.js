76816870d777f4f1d81520a3afd01ca4e3c4a338
ian <hope030327@naver.com>
2024-08-02 13:42:44 +0900

    로딩중 창 변경

--- scripts/startmainscript.js
+++ scripts/startmainscript.js
@@ -1,7 +1,8 @@
 function handleKeyPress(event) {
     if (event.code === 'Space' || event.code === 'Enter') {
-        // 엔터나 스페이스바 누르면 홈 게임화면으로 이동
-        window.location.href = 'home.html';
+        // 엔터나 스페이스바 누르면 로딩중 화면으로 이동
+        // 아직 확실히 결정된건 아님!!!!
+        window.location.href = 'loading.html';
     }
 }
 document.addEventListener('keydown', handleKeyPress);
