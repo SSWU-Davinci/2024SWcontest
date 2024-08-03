73ead5de5e157fff38c664413e5e2594b957b991
ian <hope030327@naver.com>
2024-08-04 01:10:50 +0900

    전반적인 파일 통일 + 메인 부분 연결 확인

--- scripts/criminal.js
+++ scripts/criminal.js
@@ -7,3 +7,14 @@ document.addEventListener("DOMContentLoaded", function() {
         firedText.style.opacity = 1;
     });
 });
+
+// 디렉토리로 이동하기?
+
+// 엔딩으로 이동하기
+function handleKeyPress(event) {
+    if (event.code === 'Space' || event.code === 'Enter') {
+        // 엔터키와 스페이스바 누르면 엔딩 화면으로 이동하기
+        window.location.href = 'ending.html';
+    }
+}
+document.addEventListener('keydown', handleKeyPress);
