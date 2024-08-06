9164fc9b25107a274db53231d39f8bd00616c3ac
ian <hope030327@naver.com>
2024-08-06 15:20:39 +0900

    ??수정

--- scripts/criminal.js
+++ scripts/criminal.js
@@ -8,12 +8,12 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 });
 
-// 디렉토리로 이동하기?
+// 디렉토리로 이동하기
 
 // 엔딩으로 이동하기
 function handleKeyPress(event) {
     if (event.code === 'Space' || event.code === 'Enter') {
-        // 엔터키와 스페이스바 누르면 엔딩 화면으로 이동하기
+        // 엔터키와 스페이스바 누르면 홈 화면으로 이동하기
         window.location.href = 'ending.html';
     }
 }
