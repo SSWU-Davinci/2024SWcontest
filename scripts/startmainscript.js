23fab828bc3baa40095e6e16c0e0d1bdaff3b05d
hyorim jo <133748490+hyorim-jo@users.noreply.github.com>
2024-08-06 21:45:34 +0900

    엔터키

--- scripts/startmainscript.js
+++ scripts/startmainscript.js
@@ -2,6 +2,7 @@ function handleScreenClick() {
     // 화면을 클릭하면 홈 화면으로 이동하기
     window.location.href = 'home.html';
 }
+document.addEventListener('click', handleScreenClick);
 
 function handleKeyPress(event) {
     if (event.code === 'Space' || event.code === 'Enter') {
@@ -10,5 +11,3 @@ function handleKeyPress(event) {
     }
 }
 document.addEventListener('keydown', handleKeyPress);
-
-document.addEventListener('click', handleScreenClick);
