2a609ce2ed55999c772aca61e9c7b046f9892924
hyorim jo <133748490+hyorim-jo@users.noreply.github.com>
2024-08-06 21:43:45 +0900

    엔터도 가능

--- scripts/startmainscript.js
+++ scripts/startmainscript.js
@@ -3,4 +3,12 @@ function handleScreenClick() {
     window.location.href = 'home.html';
 }
 
+function handleKeyPress(event) {
+    if (event.code === 'Space' || event.code === 'Enter') {
+        // 엔터키와 스페이스바 누르면 홈 화면으로 이동하기
+        window.location.href = 'home.html';
+    }
+}
+document.addEventListener('keydown', handleKeyPress);
+
 document.addEventListener('click', handleScreenClick);
