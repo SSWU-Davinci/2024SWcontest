451c6634935283184193583ba3b2a680047b2f1c
hyorim jo <133748490+hyorim-jo@users.noreply.github.com>
2024-08-11 20:32:13 +0900

    모든 동물 클릭 대사 수정

--- scripts/fireButton.js
+++ scripts/fireButton.js
@@ -1,62 +1,20 @@
-import { getCriminal } from './logLoad.js';
+import { setCriminal, getCriminal, getFire } from './logLoad.js';
 
 document.addEventListener('DOMContentLoaded', function () {
     const fireButton = document.getElementById('fire');
+    const dialogue = document.getElementById('dialogue'); // 대사를 표시하는 요소를 가져옵니다.
 
     fireButton.addEventListener('click', function() {
         const criminal = getCriminal();
+        const fire = getFire();
 
-        if (criminal === 0) {
-            window.location.href = 'gameover.html';
-        } else if (criminal === 1) {
-            window.location.href = 'loading.html';
+        if (fire === 1) {  // fire가 1일 때만 클릭 동작을 허용
+            if (criminal === 0) {
+                window.location.href = 'gameover.html';
+            } else if (criminal === 1) {
+                window.location.href = 'loading.html';
+            }
+        } else {
         }
     });
 });
-
-/*document.addEventListener("DOMContentLoaded", function() {
-
-const animals = document.querySelectorAll(".animal");
-const fireButton = document.getElementById('fire');
-
-let animalData = [];
-
-// JSON 데이터를 불러오기
-fetch('../public/log/log.json')
-    .then(response => response.json())
-    .then(data => {
-        animalData = data;
-
-        if (fireButton) {
-            fireButton.addEventListener('click', function() {
-                console.log("Fire button clicked");
-
-                // 선택된 동물 확인
-                const selectedAnimal = Array.from(animals).find(animal => animal.dataset.selected);
-
-                if (selectedAnimal) {
-                    // JSON 데이터에서 선택된 동물의 범인 여부 찾기
-                    const animalInfo = animalData.find(item => item.id === selectedAnimal.id);
-                    const isCriminal = animalInfo ? animalInfo.isCriminal : false;
-
-                    if (isCriminal) {
-                        // 범인 선택 시 loading.html --> criminal.html로 이동
-                        window.location.href = 'loading.html';
-                    } 
-                    else {
-                        // 잘못된 동물 선택 시 gameover.html로 이동
-                        window.location.href = 'gameover.html';
-                    }
-                } 
-                else {
-                    console.log("No animal selected");
-                }
-            });
-        } 
-        else {
-            console.error("Fire button not found");
-        }
-    })
-    .catch(error => console.error('Error loading JSON:', error));
-});
-*/
