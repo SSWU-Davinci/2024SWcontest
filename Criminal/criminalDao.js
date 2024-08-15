b19236ac231f491114e7bd03a68089c5434f2b38
chaeyeon <chaeyeon0792@gmail.com>
2024-08-15 17:37:49 +0900

    inventory

--- Criminal/criminalDao.js
+++ Criminal/criminalDao.js
@@ -1,6 +1,6 @@
 const pool = require('../config/dbConfig');
-
-async function addAnimalToUserCatalog(connection, user_number, floor, position) {
+// 도감에 색출해낸 범인 저장
+async function addAnimal(connection, user_number, floor, position) {
 
     // 층, 위치로 동물 번호 찾기
     const [animalRows] = await connection.execute(
@@ -12,24 +12,24 @@ async function addAnimalToUserCatalog(connection, user_number, floor, position) 
     const animal_number = animalRows[0].animal_number;
 
     // 사용자 도감에서 범인 정보 확인
-    const [catalogRows] = await connection.execute(
+    const [inventoryRows] = await connection.execute(
         `SELECT 1
          FROM user_animal
          WHERE user_number = ? AND animal_number = ?`,
         [user_number, animal_number]
     );
-
-    if (catalogRows.length > 0) {
+    // 도감에 이미 등록된 동물인 경우 에러 메세지 반환
+    if (inventoryRows.length > 0) {
         console.error('범인이 이미 도감에 등록되어 있습니다.');
         return { success: false, message: 'The animal is already in the user catalog.' };
     }
 
     // user_animal의 최대 값을 찾고 1 증가시킴
-    const [maxUserAnimalRow] = await connection.execute(
+    const [max] = await connection.execute(
         'SELECT IFNULL(MAX(user_animal_number), 0) + 1 AS new_user_animal FROM user_animal'
     );
 
-    const newUserAnimal = maxUserAnimalRow[0].new_user_animal;
+    const newUserAnimal = max[0].new_user_animal;
 
     // 도감에 범인 정보 추가 (user_animal 열 포함)
     await connection.execute(
@@ -42,7 +42,7 @@ async function addAnimalToUserCatalog(connection, user_number, floor, position) 
 }
 
 // 사용자의 도감 정보를 조회하고 script_number 반환
-async function getUserCatalog(connection, user_number) {
+async function getAnimal(connection, user_number) {
     const [rows] = await connection.execute(
         `SELECT a.animal_name
          FROM user_animal ua
@@ -51,11 +51,10 @@ async function getUserCatalog(connection, user_number) {
         [user_number]
     );
 
-    // 결과 반환
     return rows;
 }
 
 module.exports = {
-    addAnimalToUserCatalog,
-    getUserCatalog
+    addAnimal,
+    getAnimal
 };
